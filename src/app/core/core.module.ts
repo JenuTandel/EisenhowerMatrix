5afb3162bd86b856a66d8fea0124db65e3eac1bc
Jinal Tandel <jinal.tandel@1rivet.local>
2023-09-11 19:56:28 +0530

    feat: overlay service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { OverlayService } from './services/overlay.service';
 
 @NgModule({
   declarations: [HeaderComponent, ProfileComponent, NavbarComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, OverlayModule],
   exports: [HeaderComponent, ProfileComponent, NavbarComponent],
 })
 export class CoreModule {}
--- src/app/core/services/overlay.service.ts
+++ src/app/core/services/overlay.service.ts
@@ -1,9 +1,41 @@
-import { Injectable } from '@angular/core';
+import { Overlay } from '@angular/cdk/overlay';
+import { ComponentPortal } from '@angular/cdk/portal';
+import { ComponentRef, Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OverlayService {
+  public closeDialog: Subject<boolean>;
+  public instance: any;
 
-  constructor() { }
+  constructor(private overlay: Overlay) {
+    this.closeDialog = new Subject();
+  }
+
+  openDialog(component: any): ComponentRef<any> {
+    const positionStrategy = this.overlay.position().global();
+
+    // Create an overlay with customizable options
+    const overlayRef = this.overlay.create({
+      positionStrategy,
+      hasBackdrop: true,
+    });
+
+    const portal = new ComponentPortal(component);
+    this.instance = overlayRef.attach(portal);
+
+    // Close the dialog using backdropClick()
+    overlayRef.backdropClick().subscribe(() => {
+      overlayRef.detach();
+    });
+
+    this.closeDialog.subscribe((res) => {
+      if (res == true) {
+        overlayRef.detach();
+      }
+    });
+    return this.instance;
+  }
 }
