0dca8652e8107dc87625edfef292751f49884934
Jinal Tandel <jinal.tandel@1rivet.local>
2023-09-11 12:29:29 +0530

    navbar-ui

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent, ProfileComponent, NavbarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [HeaderComponent, ProfileComponent, NavbarComponent],
 })
 export class CoreModule {}
