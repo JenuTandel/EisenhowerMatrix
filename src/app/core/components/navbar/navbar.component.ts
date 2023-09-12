16fc1779464cd543b943451dd9f62d2da5c1909a
Jinal Tandel <jinal.tandel@1rivet.local>
2023-09-12 12:57:10 +0530

    navbar tab style

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  public line: boolean = true;
+  constructor(private router: Router) {}
+  onTodolist() {
+    this.router.navigateByUrl('/todo-list');
+    this.line = true;
+  }
+  onTaskpriority() {
+    this.router.navigateByUrl('/task-priority');
+    this.line = false;
+  }
 }
