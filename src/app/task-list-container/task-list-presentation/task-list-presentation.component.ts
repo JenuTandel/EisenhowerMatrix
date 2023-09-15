a44474d216db899255eb5737079cdcd2554f3c7d
Jinal Tandel <jinal.tandel@1rivet.local>
2023-09-15 19:29:21 +0530

    add task-progress

--- src/app/task-list-container/task-list-presentation/task-list-presentation.component.ts
+++ src/app/task-list-container/task-list-presentation/task-list-presentation.component.ts
@@ -1,9 +1,20 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { DataCommunicationsService } from 'src/app/core/services/data-communications.service';
 import { OverlayService } from 'src/app/core/services/overlay.service';
 import { Task } from 'src/app/models/task.model';
 import { TaskDetailsComponent } from 'src/app/task-details/task-details.component';
 import { TaskFormContainerComponent } from 'src/app/task-form-container/task-form-container.component';
+import { TaskProgressComponent } from 'src/app/task-progress/task-progress.component';
 import { TaskListPresenterService } from '../task-list-presenter/task-list-presenter.service';
 
 @Component({
@@ -12,11 +23,12 @@ import { TaskListPresenterService } from '../task-list-presenter/task-list-prese
   styleUrls: ['./task-list-presentation.component.scss'],
   providers: [TaskListPresenterService],
 })
-export class TaskListPresentationComponent {
+export class TaskListPresentationComponent implements AfterViewInit {
   @Input() allTask: any;
   @Output() deleteTask: EventEmitter<any>;
   @Output() editTask: EventEmitter<any>;
   @Output() taskDetails: EventEmitter<any>;
+  @ViewChildren('selectedValues') selectedValueElements!: QueryList<ElementRef>;
 
   constructor(
     private overlayService: OverlayService,
@@ -25,6 +37,9 @@ export class TaskListPresentationComponent {
     this.deleteTask = new EventEmitter();
     this.editTask = new EventEmitter();
     this.taskDetails = new EventEmitter();
+  }
+  ngAfterViewInit(): void {
+    let s = document.getElementById('selectedValue');
   }
   ngOnInit() {}
 
@@ -43,4 +58,55 @@ export class TaskListPresentationComponent {
     }, 0);
     this.overlayService.openDialog(TaskDetailsComponent, 'task-details');
   }
+
+  // Function to update a specific selected value
+  updateSelectedValue(index: number, value: string) {
+    console.log(value);
+
+    const selectedValueElement =
+      this.selectedValueElements.toArray()[index].nativeElement;
+    selectedValueElement.textContent = value;
+    if (value == 'Not Started') {
+      debugger;
+      selectedValueElement.classList.add('p-2', 'rounded-2', 'me-2', 'bg-gray');
+    }
+    if (value == 'In Progress') {
+      debugger;
+      selectedValueElement.classList.add(
+        'p-2',
+        'rounded-2',
+        'me-2',
+        'bg-info',
+        'text-white'
+      );
+    }
+    if (value == 'Done') {
+      debugger;
+      selectedValueElement.classList.add(
+        'py-2',
+        'px-4',
+        'rounded-2',
+        'me-2',
+        'bg-success',
+        'text-white'
+      );
+    }
+  }
+  // onDropDown(clickedSpan: HTMLSpanElement) {
+  //   const rect = clickedSpan.getBoundingClientRect();
+  //   const overlayPosition = {
+  //     top: rect.bottom + window.scrollY + 'px',
+  //     left: rect.left + 'px',
+  //   };
+
+  //   this.overlayService.openDialog(TaskProgressComponent, 'task-progress');
+  //   // Get a reference to the overlay element with the 'task-progress' class
+  //   const overlayElement = document.querySelector(
+  //     '.task-progress'
+  //   ) as HTMLElement;
+
+  //   // Apply overlayPosition values as inline styles
+  //   overlayElement.style.top = overlayPosition.top;
+  //   overlayElement.style.left = overlayPosition.left;
+  // }
 }
